cb825fcf1d19aeb595039f321fdd4c4d4ec08d57
MNA <angel.martinnavas2323@gmail.com>
2025-10-21 19:00:32 +0200

    comienzo de los ejercicios de prgraamcion ajercicio

--- Ejercicios 2/Tema2/src/EjerciciosBasicos/EjeciciosBasicos_21.java
+++ Ejercicios 2/Tema2/src/EjerciciosBasicos/EjeciciosBasicos_21.java
@@ -16,4 +16,20 @@ public class EjeciciosBasicos_21 {
         System.out.println("la media es" + suma / cantidad);
         sc.close();
     }
+
+    public static class EjerciciosBasicos_1 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("introduce numero entero");
+            int numero = sc.nextInt();
+
+            if (numero%2 ==0) {
+                System.out.println(numero + " es par");
+            }else {
+                System.out.println(numero +" es impar");
+            }
+            sc.close();
+        }
+    }
 }
