c5004a9a987a25950a1397de423218c5f01b0a80
MNA <angel.martinnavas2323@gmail.com>
2025-10-27 20:25:10 +0100

    comienzo de los ejercicios de prgraamcion ajercicio

---
+++ Ejercicios 2/Tema2/src/EjerciciosAvanzados/Avanzado3_5.java
@@ -0,0 +1,8 @@
+package EjerciciosAvanzados;
+
+public class Avanzado3_5 {
+    //Metodo factorial(), que calculará el factorial del número que se le pasa como parametro. Retornará el resultado del cálculo del factorial, o bien un -1 en caso de que el parámetro pasado sea negativo, y un -2 en caso de que el parámetro pasado sea cero.
+    public static void main(String[] args) {
+
+    }
+}
